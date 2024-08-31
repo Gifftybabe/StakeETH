f427738ad67e99e4aa25e1dffe2d62c2dc115afc
Gifftybabe <ulokangozi@gmail.com>
2024-08-31 12:09:09 +0100

    added function

--- contracts/StakeEth.sol
+++ contracts/StakeEth.sol
@@ -3,19 +3,42 @@ pragma solidity ^0.8.17;
 
 contract StakeEth {
 
-    uint256 public duration = 30; //days
+    //function stake
 
-    mapping (address => uint256) userStake;
+    //Multiple User can stake.
+    /**
+    * stakes => [user][amount, duration, interest]
+    */
 
-    //function to stake
-    function stake() external payable  {
-        require(msg.sender != address(0), "Address zero detected");
-        require(msg.value > 0, "Amount must be greater than zero.");
-        uint256 amount = msg.value;
-        userStake[msg.sender] += amount;
+    uint256 constant public MAX_DURATION = 60; //in days
+    uint256 constant public DAYS_IN_YEAR = 365;
+    uint256 constant public FIXED_RATE = 10; //RATE IN PERCENTAGE
+
+    constructor () payable {
+        
     }
-    //function  to claimReward
 
-    // function to Withdraw
 
+    struct Stake{
+        address _address;
+        uint256 endTime;
+        uint256 expectedInterest;
+        bool isComplete;
+    }
+    Stake [] stakes;
+    mapping (address => Stake[]) userStakes;
+
+    function stake () external payable {
+        require(msg.sender != address(0), "Address zero detected");
+        require(msg.value > 0, "Amount must be greater than zero");
+        // When staking calculate estimated Reward for the user 
+        Stake memory newStake = Stake({
+            _address: msg.sender,
+            endTime: block.timestamp + MAX_DURATION,
+            expectedInterest: calculateInterest(msg.value, FIXED_RATE, MAX_DURATION),
+            isComplete: false
+        });
+        stakes.push(newStake);
+        userStakes[msg.sender] = stakes; 
+    }
 }
