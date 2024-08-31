b3fa744ff4a61f51518c490c83c494ee4109ca39
Gifftybabe <ulokangozi@gmail.com>
2024-08-31 13:29:46 +0100

    reward function

--- contracts/StakeEth.sol
+++ contracts/StakeEth.sol
@@ -41,4 +41,32 @@ contract StakeEth {
         stakes.push(newStake);
         userStakes[msg.sender] = stakes; 
     }
+
+     //claimReward
+    function claimReward(address _address, uint256 _index) external payable {
+        require(userStakes[_address][_index].expectedInterest > 0, "No valid stake at the selected index");
+        Stake storage selectedStake = userStakes[_address][_index];
+        require(block.timestamp > selectedStake.endTime, "Stake is still ongoing");
+        require(!selectedStake.isComplete, "Stake already completed");
+        require(address(this).balance >= selectedStake.expectedInterest, "Contract does not have enough funds");
+        selectedStake.isComplete = true;
+        (bool success,) = msg.sender.call{value: selectedStake.expectedInterest}("");
+        require(success, "Reward transfer failed");
+    }
+
+
+    function getAllUserStakes(address _address) external view returns (Stake[] memory) {
+        require(msg.sender != address(0), "Address zero detected.");
+        require(userStakes[_address].length > 0, "User not found.");
+        return userStakes[_address];
+    }
+
+    
+    function calculateInterest(uint256 principal, uint256 rate, uint256 daysStaked) public pure returns (uint256) {
+        // Simple interest formula: Interest = P * r * t
+        // Where r = interestRate / 100 and t = daysStaked / DAYS_IN_YEAR
+        uint256 timeInYears = daysStaked * 1e18 / DAYS_IN_YEAR; // Converting to wei for precision
+        uint256 interest = (principal * rate * timeInYears) / (100 * 1e18); // Calculating interest
+        return principal + interest;
+    }
 }
